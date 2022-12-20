86013c5cc6ad1e7ee332180ba1cbc2e64159b9ea
Thalio-Toledo <thalio.speed@hotmail.com>
2022-12-19 21:48:05 -0300

    Primeiro commit

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    internal class ContaCorrente
+
+
+    {
+
+       public int numero_agencia;
+       public string conta;
+       public string titular;
+       public double saldo;
+
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+
+Console.WriteLine("Saldo da conta do " + contaDoAndre.titular + " é " + contaDoAndre.saldo);
